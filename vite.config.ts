b1be4e671e3c595aff82cd3cf973110448ed45a2
developmkaitech <develop.mk.aitech@gmail.com>
2025-12-15 17:20:46 +0000

    another change

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // 'base' is crucial for GitHub Pages. './' makes assets relative so it works on any repo name.
-  base: './', 
+  base: '/test_copy_x/', 
 })
